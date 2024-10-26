4d2347502d10f62fa54ff5240e3ab12910c857f0
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-10-25 23:43:36 -0300

    feat: add FeaturedTool component and featuredTools array to display tools on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,40 @@
 import React from "react";
+import ToolImage from "../assets/react.svg";
 
 const HomePage = () => {
+  const FeaturedTool = ({ image, category, title }) => (
+    <div className="">
+      <img src={image} />
+      <div className="">
+        <span>{title}</span>
+        <span>{category}</span>
+      </div>
+    </div>
+  );
+
+  const featuredTools = [
+    {
+      image: ToolImage,
+      category: "Categoria 1",
+      title: "Ferramenta 1",
+    },
+    {
+      image: ToolImage,
+      category: "Categoria 2",
+      title: "Ferramenta 2",
+    },
+    {
+      image: ToolImage,
+      category: "Categoria 3",
+      title: "Ferramenta 3",
+    },
+    {
+      image: ToolImage,
+      category: "Categoria 4",
+      title: "Ferramenta 4",
+    },
+  ];
+
   return (
     <>
       <main>
@@ -18,27 +52,48 @@ const HomePage = () => {
         </section>
         <section>
           <div className="wrapper">
-            <div className="flex items-center justify-center gap-2">
-              <button className="text-xl text-black bg-white px-4 py-2 rounded-xl">Todas</button>
+            <div className="flex items-center justify-center gap-2 mb-8">
+              <button className="text-xl text-black bg-white px-4 py-2 rounded-xl">
+                Todas
+              </button>
               <ul className="flex items-center text-xl border border-neutral-700 rounded-xl">
                 <li className="flex items-center gap-2 px-5 py-2">
-                  <a href="" className="hover:text-neutral-400">Categoria 1</a>
+                  <a href="" className="hover:text-neutral-400">
+                    Categoria 1
+                  </a>
                 </li>
                 <li className="flex items-center gap-2 px-5 py-2">
-                  <a href="" className="hover:text-neutral-400">Categoria 2</a>
+                  <a href="" className="hover:text-neutral-400">
+                    Categoria 2
+                  </a>
                 </li>
                 <li className="flex items-center gap-2 px-5 py-2">
-                  <a href="" className="hover:text-neutral-400">Categoria 3 </a>
+                  <a href="" className="hover:text-neutral-400">
+                    Categoria 3{" "}
+                  </a>
                 </li>
                 <li className="flex items-center gap-2 px-5 py-2">
-                  <a href="" className="hover:text-neutral-400">Categoria 4</a>
+                  <a href="" className="hover:text-neutral-400">
+                    Categoria 4
+                  </a>
                 </li>
                 <li className="flex items-center gap-2 px-5 py-2">
-                  <a href="" className="hover:text-neutral-400">Categoria 5</a>
+                  <a href="" className="hover:text-neutral-400">
+                    Categoria 5
+                  </a>
                 </li>
               </ul>
             </div>
-            <div></div>
+            <div className="grid grid-cols-4 gap-4 mb-16">
+              {featuredTools.map((post, index) => (
+                <FeaturedTool
+                  key={index}
+                  image={post.image}
+                  category={post.category}
+                  title={post.title}
+                />
+              ))}
+            </div>
           </div>
         </section>
       </main>
