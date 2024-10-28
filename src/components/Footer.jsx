475980fd71ea8e298878df19dfbce5371fb00591
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-10-27 23:59:10 -0300

    update: about page content and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,12 @@ const Footer = () => {
     <footer className="border-t border-t-white border-opacity-20">
       <div className="wrapper flex justify-between pt-16 pb-8">
         <div className="flex flex-col justify-between">
-          <a href="" className="text-4xl font-bold uppercase">
+          <Link to="/" className="text-4xl font-bold uppercase">
             Frontend Tools
-          </a>
+          </Link>
           <div>
             Coded by{" "}
-            <a href="https://snayderrodrigues.vercel.app/">Snayder Rodrigues</a>
+            <a href="https://snayderrodrigues.vercel.app/" target="_blank">Snayder Rodrigues</a>
           </div>
         </div>
         <div className="flex flex-col gap-4">
