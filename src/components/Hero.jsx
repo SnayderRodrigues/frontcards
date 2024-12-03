4373ab7bd2cc3fbe5a410bf3835c23cf1fa9f52b
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-12-02 23:59:34 -0300

    update: Add responsive design for hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ const Hero = () => {
   return (
     <>
       <section className="flex items-center">
-        <div className="wrapper flex flex-col pt-48 pb-32">
-          <h1 className="flex max-w-[18ch] leading-tight mb-16">
+        <div className="wrapper flex flex-col pb-24 pt-48 xl:pb-32">
+          <h1 className="flex max-w-[18ch] leading-tight mb-8 lg:mb-12 xl:mb-16">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit.
           </h1>
-          <p className="text-2xl max-w-[48ch]">
+          <p className="text-base sm:text-lg lg:text-xl xl:text-2xl max-w-[48ch]">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur,
             eaque. Voluptatem voluptas quaerat, similique repellendus.
           </p>
