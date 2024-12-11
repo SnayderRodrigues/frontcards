54a391d938631472f9282285fc12dadcc1bf23ce
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-12-11 15:12:40 -0300

    fix: Add new Header.jsx component with bug fixes

--- src/app/error/Page.jsx
+++ src/app/error/Page.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-// import Header from "../../components/Header";
+import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
 const ErrorPage = () => {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <main>
         <div className="wrapper py-32">
           <h1>Not Found</h1>
