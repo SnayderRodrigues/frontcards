2f2d989abbba476bac3ae6784073cf5d8a5f758e
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-12-14 19:00:50 -0300

    feat: Add toggle behavior for category and pagination panels in the Filter component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -9,6 +9,17 @@ const Filter = ({
   setSelectedCategory,
 }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isPaginationOpen, setIsPaginationOpen] = useState(false);
+
+  const handleCategoryIsOpen = () => {
+    setIsOpen(!isOpen);
+    if (!isOpen) setIsPaginationOpen(false);
+  };
+
+  const handleIsPaginationOpen = () => {
+    setIsPaginationOpen(!isPaginationOpen);
+    if (!isPaginationOpen) setIsOpen(false);
+  };
 
   const categories = [
     "Todas",
@@ -43,7 +54,7 @@ const Filter = ({
       </ul>
       <button
         className="flex-1 md:hidden flex items-center gap-2 p-2 border border-neutral-700 rounded-xl"
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={handleCategoryIsOpen}
       >
         <span className="flex-1 flex bg-neutral-100 text-black px-2 py-1 rounded-lg cursor-pointer transition-colors">
           {selectedCategory}
@@ -68,7 +79,7 @@ const Filter = ({
         {categories.map((category) => (
           <li
             key={category}
-            className={`flex items-center gap-2 px-2 py-1 lg:px-3 lg:py-1 xl:px-5 xl:py-2 rounded-lg cursor-pointer transition-colors ${
+            className={`flex items-center gap-2 px-2 py-1 rounded-lg cursor-pointer transition-colors ${
               selectedCategory === category
                 ? "bg-neutral-100 text-black"
                 : "hover:bg-neutral-800"
@@ -79,12 +90,50 @@ const Filter = ({
           </li>
         ))}
       </ul>
-      <div className="shrink-0 flex items-center gap-2 p-2 border border-neutral-700 rounded-xl overflow-hidden">
+      <div className="shrink-0 hidden md:flex items-center gap-2 p-2 border border-neutral-700 rounded-xl overflow-hidden">
         {Array.from({ length: totalPages }, (_, index) => (
           <button
             key={index + 1}
             onClick={() => setCurrentPage(index + 1)}
             className={`w-[32px] lg:w-[36px] xl:w-[44px] aspect-square flex items-center justify-center text-base lg:text-lg xl:text-xl font-semibold rounded-lg transition-colors ${
+              currentPage === index + 1
+                ? "bg-neutral-100 text-black"
+                : "hover:bg-neutral-800"
+            }`}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+      <button
+        className="md:hidden flex items-center gap-2 p-2 border border-neutral-700 rounded-xl"
+        onClick={handleIsPaginationOpen}
+      >
+        <span className="flex-1 flex bg-neutral-100 text-black px-3 py-1 rounded-lg cursor-pointer transition-colors">
+          {currentPage}
+        </span>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20px"
+          height="20px"
+          viewBox="0 -960 960 960"
+          fill="#fff"
+        >
+          <path d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z" />
+        </svg>
+      </button>
+      <div
+        className={`shrink-0 w-full absolute bottom-[58px] z-10 flex md:hidden flex-col gap-2 bg-neutral-950 p-2 border border-neutral-700 rounded-xl transition-opacity overflow-hidden ${
+          isPaginationOpen ? "" : "opacity-0 select-none pointer-events-none"
+        }`}
+        onClick={() => setIsPaginationOpen(!isPaginationOpen)}
+      >
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => setCurrentPage(index + 1)}
+            className={`w-full flex text-base px-2 py-1 font-semibold rounded-lg transition-colors ${
               currentPage === index + 1
                 ? "bg-neutral-100 text-black"
                 : "hover:bg-neutral-800"
