163a7b6c567faa436138089eebb73ed0bc450fe5
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-12-21 20:40:27 -0300

    update: About page styles and responsive design

--- src/app/about/Page.jsx
+++ src/app/about/Page.jsx
@@ -1,59 +1,49 @@
 import React from "react";
-import AuthorImage from "../../assets/react.svg";
+import AuthorImage from "../../../public/author-photo.webp";
 
 const AboutPage = () => {
   return (
     <>
       <section className="font-medium">
-        <div className="wrapper grid grid-cols-2 gap-32 pt-48">
-          <h1 className="text-6xl font-bold uppercase">
-            Sobre <br />{" "}
-            <span className="text-neutral-400">Frontend Tools</span>
+        <div className="wrapper pt-32 sm:pt-36 lg:pt-40 xl:pt-48 pb-12 md:pb-16 lg:pb-32">
+          <h1 className="leading-tight font-bold mb-8 lg:mb-12 xl:mb-16">
+            Sobre <br /> <span className="text-indigo-400">Frontend Tools</span>
           </h1>
-          <div className="flex flex-col gap-8 text-2xl text-neutral-200 max-w-[60ch]">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-              voluptatibus minima fuga ipsa harum consectetur iste sunt. Officia
-              ab explicabo, facilis ut ad dolore error quo quas commodi eaque
-              quidem.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-              voluptatibus minima fuga ipsa harum consectetur iste sunt. Officia
-              ab explicabo, facilis ut ad dolore error quo quas commodi eaque
-              quidem.
-            </p>
-          </div>
-        </div>
-      </section>
-      <section className="font-medium">
-        <div className="wrapper grid grid-cols-2 gap-32 pt-48 pb-32">
-          <div className="flex flex-col gap-8 text-2xl text-neutral-200 max-w-[60ch]">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-              voluptatibus minima fuga ipsa harum consectetur iste sunt. Officia
-              ab explicabo, facilis ut ad dolore error quo quas commodi eaque
-              quidem.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-              voluptatibus minima fuga ipsa harum consectetur iste sunt. Officia
-              ab explicabo, facilis ut ad dolore error quo quas commodi eaque
-              quidem.
-            </p>
-            <div className="text-base flex items-center gap-4">
-              <img src={AuthorImage} alt="" />
-              <div className="flex flex-col text-2xl">
-                <span className="text-white">Snayder Rodrigues</span>
-                <span>Frontend Developer</span>
+          <div className="flex items-center gap-16">
+            <div className="flex flex-col gap-8 text-base sm:text-lg lg:text-xl xl:text-2xl text-neutral-200 max-w-[60ch]">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+                voluptatibus minima fuga ipsa harum consectetur iste sunt.
+                Officia ab explicabo, facilis ut ad dolore error quo quas
+                commodi eaque quidem.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                minima et ipsum architecto cupiditate blanditiis sint,
+                voluptates similique pariatur, ducimus deleniti doloremque!
+                Asperiores, ex aliquam rerum id a consectetur saepe?
+              </p>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Voluptates laborum possimus eveniet vel amet inventore magni
+                dignissimos in alias debitis non officia, necessitatibus sequi,
+                eaque beatae consectetur! Facilis, incidunt aperiam!
+              </p>
+              <div className="flex items-center gap-4">
+                <img
+                  src={AuthorImage}
+                  alt=""
+                  className="w-12 aspect-square rounded-full"
+                />
+                <div className="flex flex-col">
+                  <span>Snayder Rodrigues</span>
+                  <span className="text-neutral-400">
+                    Desenvolvedor Frontend
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-          <img
-            src={AuthorImage}
-            alt=""
-            className="w-full max-h-96 border-[4px] border-neutral-700 rounded-2xl"
-          />
         </div>
       </section>
     </>
