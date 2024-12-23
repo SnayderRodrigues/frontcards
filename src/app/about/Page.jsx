b0cbec6bfc543bfaf7afae2de0eca2cc78c88e9d
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-12-22 23:43:34 -0300

    update: Hero.jsx component and About Page styles and responsive design

--- src/app/about/Page.jsx
+++ src/app/about/Page.jsx
@@ -5,7 +5,7 @@ const AboutPage = () => {
   return (
     <>
       <section className="font-medium">
-        <div className="wrapper pt-32 sm:pt-36 lg:pt-40 xl:pt-48 pb-12 md:pb-16 lg:pb-32">
+        <div className="wrapper pt-32 sm:pt-36 lg:pt-36 xl:pt-40 pb-12 md:pb-16 lg:pb-32">
           <h1 className="leading-tight font-bold mb-8 lg:mb-12 xl:mb-16">
             Sobre <br /> <span className="text-indigo-400">Frontend Tools</span>
           </h1>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ const Hero = () => {
   return (
     <>
       <section className="flex items-center" id="hero-section">
-        <div className="wrapper flex flex-col pt-32 sm:pt-36 lg:pt-40 xl:pt-48 pb-12 xsm:pb-20 md:pb-24 xl:pb-32">
-          <h1 className="max-w-[19ch] leading-tight mb-8 lg:mb-12 xl:mb-16">
+        <div className="wrapper flex flex-col pt-32 sm:pt-36 lg:pt-36 xl:pt-40 pb-12 xsm:pb-16 md:pb-20">
+          <h1 className="max-w-[19ch] leading-tight mb-8 lg:mb-12">
             Recursos e ferramentas para <span className="text-indigo-400">Desenvolvedores Frontend</span> e <span className="text-purple-400">
               Web
               Designers
