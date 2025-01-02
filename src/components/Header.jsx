e2d7f45f14d502a3180059366b01444d6fe42036
Snayder Rodrigues <snayderrodrigues@gmail.com>
2025-01-02 16:38:26 -0300

    feat: Prevent page scrolling while hamburger menu is set to open

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Button from "./Button";
-import Logo from "/public/logo.svg";
+import Logo from "/logo.svg";
 
 const Header = () => {
   const [openMenu, setOpenMenu] = useState(false);
@@ -17,9 +17,26 @@ const Header = () => {
 
   const scrollToTop = () => window.scrollTo({ top: 0 });
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 767 && openMenu) {
+        setOpenMenu(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [openMenu]);
+
+  useEffect(() => {
+    const html = document.querySelector("html");
+    html.style.overflow = openMenu ? "hidden" : "auto";
+  }, [openMenu]);
+
   return (
     <header className="fixed z-50 w-full flex items-center justify-center bg-neutral-950 border-b-2 border-b-neutral-800">
-      <nav className="wrapper flex items-center justify-between gap-4 py-4">
+      <nav className="wrapper overfl flex items-center justify-between gap-4 py-4">
         <a href="/" className="relative z-50 flex items-center gap-2 xsm:gap-3">
           <img src={Logo} alt="Logo" width={40} className="w-[36px] xsm:w-10" />
           <div className="text-[28px] xsm:text-3xl lg:text-4xl font-bold">
