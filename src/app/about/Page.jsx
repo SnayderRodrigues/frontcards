20d04acfc5262ef295ff5c6d3b1d3d357d9f3ce4
Snayder Rodrigues <snayderrodrigues@gmail.com>
2025-01-19 23:50:41 -0300

    update: About Page text

--- src/app/about/Page.jsx
+++ src/app/about/Page.jsx
@@ -7,31 +7,23 @@ const AboutPage = () => {
         <div className="wrapper pt-32 sm:pt-36 lg:pt-36 xl:pt-40 pb-12 md:pb-16 lg:pb-24">
           <h1 className="mb-8 lg:mb-12">Sobre Frontcards</h1>
           <div className="flex items-center gap-16">
-            <div className="flex flex-col gap-8 text-base sm:text-lg lg:text-xl xl:text-2xl text-neutral-200 max-w-[60ch]">
+            <div className="max-w-[48ch] flex flex-col gap-8 text-base sm:text-lg lg:text-xl xl:text-2xl text-pretty text-neutral-200">
               <p>
-                O Frontcards nasceu com um propósito simples e significativo:
-                ajudar desenvolvedores frontend e web designers, principalmente
-                aqueles que estão começando suas jornadas, a encontrarem as
-                melhores ferramentas, criadores de conteúdo, recursos de
-                aprendizagem e muito mais.
+                O Frontcards foi desenvolvido com um propósito simples: ajudar
+                desenvolvedores frontend e web designers, principalmente aqueles
+                que estão começando suas jornadas, a encontrarem ferramentas,
+                conteúdos e recursos de aprendizagem que ajudam a potencializar
+                suas habilidades.
               </p>
               <p>
-                Inspirada no incrível projeto Pillarstack, criado por Huy
-                Nguyen, o Frontcards segue a mesma filosofia de ser uma ponte
-                entre a comunidade e os recursos mais úteis para o
-                desenvolvimento e design. Aqui, você encontrará uma seleção
-                cuidadosamente curada e frequentemente atualizada de ferramentas
-                e conteúdos para potencializar suas habilidades.
-              </p>
-              <p>
-                Nosso objetivo é criar um espaço acessível e confiável para a
-                comunidade de desenvolvimento e design, facilitando o acesso a
-                recursos de qualidade que podem ajudar desde pequenos projetos
-                pessoais até grandes desafios profissionais.
+                Aqui, uma seleção de ferramentas e recursos foi cuidadosamente
+                escolhida por mim e por outros colaboradores com o objetivo de
+                criar um espaço acessível e confiável para a comunidade web,
+                facilitando o acesso a recursos de qualidade.
               </p>
               <div className="flex flex-col">
                 <span>Snayder Rodrigues</span>
-                <span className="text-neutral-400">Criador Frontcards</span>
+                <span className="text-neutral-400">Criador do Frontcards</span>
               </div>
             </div>
           </div>
