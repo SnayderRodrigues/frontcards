11d93e058113c624973192e863a2078ab6791d42
Snayder Rodrigues <snayderrodrigues@gmail.com>
2025-01-19 23:55:10 -0300

    update: Add Legal Page text

--- src/app/about/Page.jsx
+++ src/app/about/Page.jsx
@@ -5,9 +5,9 @@ const AboutPage = () => {
     <>
       <section className="font-medium">
         <div className="wrapper pt-32 sm:pt-36 lg:pt-36 xl:pt-40 pb-12 md:pb-16 lg:pb-24">
-          <h1 className="mb-8 lg:mb-12">Sobre Frontcards</h1>
+          <h1 className="mb-8 lg:mb-12">Sobre o Frontcards</h1>
           <div className="flex items-center gap-16">
-            <div className="max-w-[48ch] flex flex-col gap-8 text-base sm:text-lg lg:text-xl xl:text-2xl text-pretty text-neutral-200">
+            <div className="max-w-[48ch] flex flex-col gap-8 text-base sm:text-lg lg:text-xl xl:text-2xl text-pretty text-neutral-400">
               <p>
                 O Frontcards foi desenvolvido com um propósito simples: ajudar
                 desenvolvedores frontend e web designers, principalmente aqueles
@@ -22,8 +22,8 @@ const AboutPage = () => {
                 facilitando o acesso a recursos de qualidade.
               </p>
               <div className="flex flex-col">
-                <span>Snayder Rodrigues</span>
-                <span className="text-neutral-400">Criador do Frontcards</span>
+                <span className="text-white">Snayder Rodrigues</span>
+                <span>Criador do Frontcards</span>
               </div>
             </div>
           </div>
--- src/app/legal/Page.jsx
+++ src/app/legal/Page.jsx
@@ -5,34 +5,34 @@ const LegalPage = () => {
     <>
       <section className="font-medium">
         <div className="wrapper pt-32 sm:pt-36 lg:pt-36 xl:pt-40 pb-12 md:pb-16 lg:pb-24">
-          <h1 className="mb-8 lg:mb-12">
-            Termos Legais
-          </h1>
+          <h1 className="mb-8 lg:mb-12">Termos Legais</h1>
           <div className="flex items-center gap-16">
-            <div className="flex flex-col gap-8 text-base sm:text-lg lg:text-xl xl:text-2xl text-neutral-200 max-w-[60ch]">
+            <div className="max-w-[48ch] flex flex-col gap-8 text-base sm:text-lg lg:text-xl xl:text-2xl text-neutral-400">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-                voluptatibus minima fuga ipsa harum consectetur iste sunt.
-                Officia ab explicabo, facilis ut ad dolore error quo quas
-                commodi eaque quidem.
+                Todas as ferramentas, logos, marcas e imagens mencionadas ou
+                exibidas neste site são propriedades de seus respectivos
+                proprietários. O Frontcards não reivindica nenhum tipo de
+                propriedade sobre elas.
               </p>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-                minima et ipsum architecto cupiditate blanditiis sint,
-                voluptates similique pariatur, ducimus deleniti doloremque!
-                Asperiores, ex aliquam rerum id a consectetur saepe?
+                O Frontcards não possui qualquer vínculo, parceria ou afiliação
+                com as ferramentas ou empresas mencionadas no site. Nosso único
+                objetivo é compartilhar recomendações com base na nossa
+                experiência e pesquisa, para ajudar outros desenvolvedores e
+                designers.
               </p>
               <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Voluptates laborum possimus eveniet vel amet inventore magni
-                dignissimos in alias debitis non officia, necessitatibus sequi,
-                eaque beatae consectetur! Facilis, incidunt aperiam!
+                As recomendações disponibilizadas no Frontcards são realizadas
+                de forma independente e com a intenção de fornecer informações
+                úteis. Não recebemos compensações financeiras ou benefícios
+                diretos por mencionar qualquer ferramenta ou serviço.
               </p>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Praesentium quod eveniet officiis quo est facere labore ipsam.
-                Debitis voluptate consequatur ut obcaecati voluptatibus nobis
-                nulla nihil, illum odit tempore magni.
+                O Frontcards não se responsabiliza por possíveis problemas,
+                danos ou prejuízos decorrentes do uso das ferramentas ou
+                serviços mencionados no site. Recomendamos que você leia os
+                termos e condições de cada ferramenta diretamente no site
+                oficial dos respectivos proprietários antes de utilizá-las.
               </p>
             </div>
           </div>
